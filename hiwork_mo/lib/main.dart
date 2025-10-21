71d9fb541e7e91748e219a8a84763a48b68672ca
Lâm Chính Nghi <lam91391@gmail.com>
2025-10-21 13:18:58 +0700

    feat: implement translate

--- hiwork_mo/lib/main.dart
+++ hiwork_mo/lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hiwork_mo/core/constants/app_colors.dart';
-import 'package:hiwork_mo/presentation/pages/splash/splash_page.dart';
+import 'package:hiwork_mo/presentation/bloc/language/language_bloc.dart';
+import 'package:hiwork_mo/presentation/bloc/language/language_state.dart';
 import 'package:hiwork_mo/presentation/route/app_route.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  runApp(
+    BlocProvider(
+      create: (_) => LanguageBloc(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,15 +23,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HiWork',
-      theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: AppColors.backgroundColor,
-      ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: AppRoute.splash,
-      onGenerateRoute: AppRoute.generateRoute,
+    return BlocBuilder<LanguageBloc, LanguageState>(
+      builder: (context, state) {
+        return MaterialApp(
+          title: 'HiWork',
+          theme: ThemeData(
+            useMaterial3: true,
+            scaffoldBackgroundColor: AppColors.backgroundColor,
+          ),
+          debugShowCheckedModeBanner: false,
+          initialRoute: AppRoute.splash,
+          onGenerateRoute: AppRoute.generateRoute,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en'), // Tiếng Anh
+            Locale('vi'), // Tiếng Việt
+          ],
+          locale: state.locale,
+        );
+      },
     );
   }
 }
