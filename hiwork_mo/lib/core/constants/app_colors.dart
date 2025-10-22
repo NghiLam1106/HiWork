e77b3653fc803380af29ff98984bbd1aa677675e
Lâm Chính Nghi <lam91391@gmail.com>
2025-10-22 17:39:22 +0700

    chore: update app_colors.dart

--- hiwork_mo/lib/core/constants/app_colors.dart
+++ hiwork_mo/lib/core/constants/app_colors.dart
@@ -1,6 +1,17 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class AppColors {
   // --------------------- BACKGROUND --------------------- //
   static const Color backgroundColor = Color(0xFFFFFFFF);
+
+  //------------------ Text ------------------//
+  static const Color textBlack = Color(0xFF000000);
+  static const Color textWhite = Color(0xFFFFFFFF);
+  static const Color textBlue = Color(0xFF007BFF);
+  static const Color textGrey = Colors.grey;
+
+  //------------------ Notification ------------------//
+  static const Color success = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFFA500);
+  static const Color error = Color(0xFFE63946);
 }
